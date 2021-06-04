db75f0206560512d43c9f34133e9d20e695ecf35
Alexander Shcherbanyuk <s0ul@protonmail.com>
2021-06-04 15:02:25 +0700

    Added API-docs, envs and README

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,9 +1,26 @@
+import json
+import yaml
+
 from django.conf import settings
+from django.shortcuts import render
 from django.urls import include, path
 
 from apps import api
 
-urlpatterns = [path('api/', include(api.urls, namespace='api'))]
+
+def render_yaml_to_swagger(request):
+    with open(settings.SWAGGER_YAML_FILE) as swagger_doc:
+        doc = yaml.load(swagger_doc.read())
+    return render(
+        request, template_name='swagger_base.html',
+        context={'data': json.dumps(doc)},
+    )
+
+
+urlpatterns = [
+    path('api/', include(api.urls, namespace='api')),
+    path('docs/', render_yaml_to_swagger, name='api-doc'),
+]
 
 if settings.DEBUG:
     from debug_toolbar import urls
